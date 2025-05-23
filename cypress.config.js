cd3ad4637bc650cc54b151544f8efaa8c01873e5
Evgeniy Augul <evgeniy.augul@1pt.com>
2025-05-23 17:30:53 +0300

    Features

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,16 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    require('mochawesome/addContext')(on);
     },
     testIsolation: false,
     defaultCommandTimeout: 7000
+  },
+    reporter: 'mochawesome',
+    reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
   }
 });

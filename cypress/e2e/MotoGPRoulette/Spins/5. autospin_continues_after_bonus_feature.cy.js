99b247868e9b38c39e08308bc8a2fa05dc6ea4d5
Evgeniy Augul <evgeniy.augul@1pt.com>
2025-05-27 13:56:44 +0300

    Spins

--- cypress/e2e/MotoGPRoulette/Spins/5. autospin_continues_after_bonus_feature.cy.js
+++ cypress/e2e/MotoGPRoulette/Spins/5. autospin_continues_after_bonus_feature.cy.js
@@ -4,7 +4,7 @@ describe('Autospin stops on Bonus Feature and Bonus Round is played without issu
         cy.visitTestEnvironment()
         cy.intercept('startgame', { fixture: 'only_one_bonus_spin_roulette.json'}).as('startGame')
 
-        cy.window({ timeout: 25000 }).should((win) => {
+        cy.window({ timeout: 50000 }).should((win) => {
             const start_button = win.game.scene.scenes[1].gameContainer.list[5].list[0].visible;
             expect(start_button, 'Game is loaded').to.be.true
         })

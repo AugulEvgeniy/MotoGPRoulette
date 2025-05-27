d9a14fa693c092f5bbb568626cc505d1b533d974
Evgeniy Augul <evgeniy.augul@1pt.com>
2025-05-27 12:43:14 +0300

    Burger

--- cypress/e2e/MotoGPRoulette/Burger_Menu/3. back_to_lobby_button.cy.js
+++ cypress/e2e/MotoGPRoulette/Burger_Menu/3. back_to_lobby_button.cy.js
@@ -4,7 +4,7 @@ describe('Back to lobby button', () => {
         cy.visitTestEnvironment()
         cy.intercept('abeta.html').as('lobby')
 
-        cy.window({ timeout: 25000 }).should((win) => {
+        cy.window({ timeout: 50000 }).should((win) => {
             const start_button = win.game.scene.scenes[1].gameContainer.list[5].list[0].visible;
             expect(start_button, 'Game is loaded').to.be.true
         })

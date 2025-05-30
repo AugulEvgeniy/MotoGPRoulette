34db9578b904155b952384de92a062140fa47c19
Evgeniy Augul <evgeniy.augul@1pt.com>
2025-05-30 15:26:03 +0300

    Burger

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,20 @@
 const { defineConfig } = require("cypress");
+const addContext = require('mochawesome/addContext');
+
 
 module.exports = defineConfig({
   videoCompression: false,
   projectId: 'ch1gds',
   e2e: {
     setupNodeEvents(on, config) {
+      process.env.TERM = 'dumb';
       on('task', {
         logCatch(message) {
           console.log(message); // Handles all log messages
           return null;
         }}),
-      // implement node event listeners here
-    require('mochawesome/addContext')(on);
+        addContext(on);
+        return config;
     },
     testIsolation: false,
     defaultCommandTimeout: 7000,

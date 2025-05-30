7bf076fdf49fb781a59bde4f4b5e9ca62c8808d9
Evgeniy Augul <evgeniy.augul@1pt.com>
2025-05-30 15:31:59 +0300

    Burger

--- cypress.config.js
+++ cypress.config.js
@@ -7,14 +7,15 @@ module.exports = defineConfig({
   projectId: 'ch1gds',
   e2e: {
     setupNodeEvents(on, config) {
-      process.env.TERM = 'dumb';
+      process.env.TERM = 'xterm';
       on('task', {
         logCatch(message) {
           console.log(message); // Handles all log messages
           return null;
         }}),
-        addContext(on);
-        return config;
+      // implement node event listeners here
+    addContext(on);
+    return config;
     },
     testIsolation: false,
     defaultCommandTimeout: 7000,

075b22feafc46209a83a946c9d5c4f1a836b7240
AugulEvgeniy <augul.evgeniy@gmail.com>
2025-06-17 11:47:19 +0300

    commit

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('visitTestEnvironment', () => {
   const testUrl = 'http://highlight.spinberry.com/applepen/HG_MotoGP_Roulette/index.html?go=dev&serverAddress=https://riw-dev.olsworth.com&productId=riwo-extern-1pt&token=123456&currency=GBP&lang=en&testConfig=local&forceDevice=tablet&hideCurrency=false'
-  cy.visit(testUrl, { timeout: 50000 })
+  cy.visit(testUrl, { timeout: 200000 })
 })
 
 Cypress.Commands.add('interceptStartGame', () => {

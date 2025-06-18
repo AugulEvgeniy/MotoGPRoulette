88e432e02885577880919a3c129e5892a7467660
AugulEvgeniy <augul.evgeniy@gmail.com>
2025-06-18 17:49:15 +0300

    commit

--- cypress/e2e/MotoGPRoulette/Test/bonus + autospin_stop_on_bonus_feature.cy.js
+++ cypress/e2e/MotoGPRoulette/Test/bonus + autospin_stop_on_bonus_feature.cy.js
@@ -99,9 +99,9 @@ describe('Autospin stops on Bonus Feature and Bonus Round is played without issu
             }   
         })
         
-        cy.window({timeout: 60000}).should((win) => {
+        cy.window({timeout: 150000}).should((win) => {
             const win_banner = win.game.scene.scenes[1].gameContainer.winBanner.visible;
-            expect(win_banner).to.be.true
+            expect(win_banner, "win banner is displayed").to.be.true
         })
 
         cy.window().then((win) => {
